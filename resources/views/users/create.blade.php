dfe4b3e2c3b1265fb246f377e49ef27f0c06aceb
Eric Priemer <elpanchopriemer@hotmail.com>
2022-08-02 18:13:41 -0300

    L8.0 y agregado de recaptcha en registrar usuario

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -19,6 +19,7 @@
 
 			<form role="form" method="POST" enctype="multipart/form-data" action="{{ route('user.postcreate') }}">
 				<input type="hidden" name="_token" value="{{ csrf_token() }}">
+				{!! htmlScriptTagJsApi(['action' => 'homepage']) !!}
 
 				<div class="form-group">
 					<label class="control-label">@lang('validation.attributes.name')</label>
@@ -50,9 +51,9 @@
 					<input type="password" class="form-control" name="password_confirmation">
 				</div>
 
-				<div class="form-group">
+				{{-- <div class="form-group">
 					{!! Recaptcha::render() !!}
-				</div>
+				</div> --}}
 
 				<div class="form-group">
 					<button type="submit" class="btn btn-primary">
