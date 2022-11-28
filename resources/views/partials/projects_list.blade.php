b559e286f9c96249c10f7bbab3d079c1fa7edb8f
Eric Priemer <elpanchopriemer@hotmail.com>
2022-11-28 20:03:09 -0300

    comienzo a arreglar el disenio de los tags

--- resources/views/partials/projects_list.blade.php
+++ resources/views/partials/projects_list.blade.php
@@ -9,16 +9,16 @@
 					<img class="card-img-top img-fluid" align="center" src="/images/proposal.jpg" alt="Card image cap">
 			    	<div class="card-block">
 						<div class="row">
-							<div class="col-md-8">
+							<div class="col-md-6">
 								<span style="color: red; margin-top:5px">{{$projects[$j]->countdown()}} Dias restantes</span>
 							</div>
-							<div class="col-md-2 col-md-offset-2">
-								<span class="tag tag-default"> 
-									@foreach ($projects[$j]->projectTag as $tag)
-										<a href="{{ route('projectTag', ['tag' => $tag->tag]) }}">{{$tag->tag->name}}</a>
-										<br>
-									@endforeach
-								</span>
+							<div class="col-md-4 col-md-offset-2">
+								@foreach ($projects[$j]->projectTag as $tag)
+									<span class="tag tag-default" href="{{ route('projectTag', ['tag' => $tag->tag]) }}"> 
+										{{$tag->tag->name}}
+									</span>
+									<br>
+								@endforeach
 							</div>
 						</div>
 			    		<h3 class="card-title" style="color: black; margin-top:10px">{{ $projects[$j]->name }}</h3>
